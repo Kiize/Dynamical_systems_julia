3602d03acee31768bb28275dcb63966a15df94f5
Kiize <vitogallo99@gmail.com>
2023-06-26 11:46:11 +0200

    update Fermi Ulam

--- Fermi_Ulam.jl
+++ Fermi_Ulam.jl
@@ -1,6 +1,8 @@
 using DynamicalSystems
-using GLMakie
+using CairoMakie
 using DynamicalSystemsBase
+
+#we choose CairoMakie as the backend because of the high resolution of the images
 
 """
 u : initial state
@@ -24,7 +26,7 @@ In this case we want to evolve a grid (u0s) of initial condition with the Fermi 
 
 #start and stop must be real numbers
 start = 1. 
-stop = 2.
+stop = pi
 step = 0.1
 u0s = [[x, y] for x ∈ start:step:stop for y ∈ start:step:stop]
 fig = Figure()
@@ -34,10 +36,10 @@ function fx(u0)
     fu_map = DiscreteDynamicalSystem(Fermi_Ulam_map, u0, ϵ)
     fu_traj = trajectory(fu_map, 1000)
     x, y = columns(fu_traj[1])
-    return scatter!(x, y, markersize = 2) 
+    return scatter!(y, x, markersize = 2) #in this way we have the thetas on the x-axis and the us on the y-axis
 end
 
 fx.(u0s)
 
 display(fig)
-save("Fermi_Ulam_map.png", fig)
+save("Fermi_Ulam_map.pdf", fig)
